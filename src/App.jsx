80898daa2bded06ed12fd2a699e31027f9c9fe1f
Ronald Paek <ronaldpaek@gmail.com>
2023-08-27 06:24:40 -0700

    Update basic html structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,19 @@ const App = () => {
       </header>
 
       <div className="search-bar">
-        <img className="search-icon" src="/icon-search.svg" alt="Search icon" />
-        <input
-          className="search-input"
-          type="search"
-          name="q"
-          placeholder="Search GitHub username..."
-        />
+        <div className="search-input-container">
+          <img
+            className="search-icon"
+            src="/icon-search.svg"
+            alt="Search icon"
+          />
+          <input
+            className="search-input truncate-text"
+            type="search"
+            name="q"
+            placeholder="Search GitHub username..."
+          />
+        </div>
         <button className="search-button">Search</button>
       </div>
 
