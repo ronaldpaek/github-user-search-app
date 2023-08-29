86802c64c8a92d8de7144d09a91c2fef59984343
Ronald Paek <ronaldpaek@gmail.com>
2023-08-29 03:42:03 -0700

    Add clear logic on SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 const SearchBar = ({ setUser }) => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [message, setMessage] = useState(null);
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    setMessage(null);
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery === "") {
+      setMessage(null);
+    }
+  }, [searchQuery]);
 
   const fetchUserData = async (username) => {
     const apiUrl = `https://api.github.com/users/${username}`;
@@ -11,35 +23,50 @@ const SearchBar = ({ setUser }) => {
       const data = await response.json();
       if (response.ok && data && data.login) {
         setUser(data);
+        setMessage(null);
+        setSearchQuery("");
       } else {
-        console.error(
-          `Failed to find a valid user. HTTP Status: ${response.status}`,
-        );
+        setMessage("No Results");
       }
     } catch (error) {
-      console.error(
-        "An error occurred while fetching the GitHub user data:",
-        error.message,
-      );
+      setMessage("An error occurred while fetching the GitHub user data");
     }
+  };
+
+  const focusInput = () => {
+    searchInputRef.current.focus();
   };
 
   const handleSearchClick = () => {
     fetchUserData(searchQuery);
   };
+
+  const handleEnterKey = (e) => {
+    if (e.key === "Enter") {
+      handleSearchClick();
+    }
+  };
   return (
     <div className="search-bar">
       <div className="search-input-container">
-        <img className="search-icon" src="/icon-search.svg" alt="Search icon" />
+        <img
+          className="search-icon"
+          src="/icon-search.svg"
+          alt="Search icon"
+          onClick={focusInput}
+        />
         <input
           className="search-input truncate-text"
+          ref={searchInputRef}
           type="search"
           value={searchQuery}
           name="q"
           placeholder="Search GitHub username..."
           onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={handleEnterKey}
         />
       </div>
+      {message && <p className="message-toast">{message}</p>}
       <button className="search-button" onClick={handleSearchClick}>
         Search
       </button>
