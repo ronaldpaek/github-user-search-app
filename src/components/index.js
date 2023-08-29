328710564a37b971979233408fec6878631a7c9f
Ronald Paek <ronaldpaek@gmail.com>
2023-08-28 21:19:10 -0700

    Update index file for components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
 import AppHeader from "./AppHeader";
 import SearchBar from "./SearchBar";
 import UserCard from "./UserCard";
+import UserBio from "./UserBio";
 import UserStats from "./UserStats";
 import UserContactDetails from "./UserContactDetails";
 
-export { AppHeader, SearchBar, UserCard, UserStats, UserContactDetails };
+export {
+  AppHeader,
+  SearchBar,
+  UserCard,
+  UserBio,
+  UserStats,
+  UserContactDetails,
+};
