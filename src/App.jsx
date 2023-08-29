db718ab17bd64692d78e866ea987e638c2faa81e
Ronald Paek <ronaldpaek@gmail.com>
2023-08-28 21:21:27 -0700

    Restructure App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,57 @@
+import { useState } from "react";
+
 import {
   AppHeader,
   SearchBar,
   UserCard,
+  UserBio,
   UserContactDetails,
   UserStats,
 } from "./components";
 import "./App.css";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "The Octocat",
+    login: "octocat",
+    html_url: "https://github.com/octocat",
+    created_at: "2011-01-25T00:00:00.000Z",
+    bio: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.",
+    public_repos: 8,
+    followers: 3938,
+    following: 9,
+    location: "San Francisco",
+    blog: "github.blog",
+    twitter_username: null,
+    company: "@github",
+  });
+  console.log({ user });
+
   return (
-    <main className="github-search-container">
+    <div className="github-search-container">
       <AppHeader />
-      <SearchBar />
-      <section className="user-section">
-        <UserCard name="The Octocat" handle="@octocat" joinDate="2011-01-25" />
-        <p className="user-bio">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
-          Quisque volutpat mattis eros.
-        </p>
-        <UserStats repos={8} followers={3938} following={9} />
-        <UserContactDetails
-          location="San Francisco"
-          website="https://github.blog"
-          twitter="@github"
-          company="@github"
+      <main>
+        <SearchBar setUser={setUser} />
+        <UserCard
+          name={user.name}
+          handle={user.login}
+          html_url={user.html_url}
+          joinDate={user.created_at}
         />
-      </section>
-    </main>
+        <UserBio bio={user.bio} />
+        <UserStats
+          repos={user.public_repos}
+          followers={user.followers}
+          following={user.following}
+        />
+        <UserContactDetails
+          location={user.location}
+          website={user.blog}
+          twitter={user.twitter_username}
+          company={user.company}
+        />
+      </main>
+    </div>
   );
 };
 
