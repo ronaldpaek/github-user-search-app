a56aed7b99a26a7fbdbefe42785450b3265c0a90
Ronald Paek <ronaldpaek@gmail.com>
2023-08-28 21:17:37 -0700

    Add fetch logic for users

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-const SearchBar = () => {
+const SearchBar = ({ setUser }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const fetchUserData = async (username) => {
+    const apiUrl = `https://api.github.com/users/${username}`;
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      if (response.ok && data && data.login) {
+        setUser(data);
+      } else {
+        console.error(
+          `Failed to find a valid user. HTTP Status: ${response.status}`,
+        );
+      }
+    } catch (error) {
+      console.error(
+        "An error occurred while fetching the GitHub user data:",
+        error.message,
+      );
+    }
+  };
+
+  const handleSearchClick = () => {
+    fetchUserData(searchQuery);
+  };
   return (
     <div className="search-bar">
       <div className="search-input-container">
@@ -7,13 +34,21 @@ const SearchBar = () => {
         <input
           className="search-input truncate-text"
           type="search"
+          value={searchQuery}
           name="q"
           placeholder="Search GitHub username..."
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
       </div>
-      <button className="search-button">Search</button>
+      <button className="search-button" onClick={handleSearchClick}>
+        Search
+      </button>
     </div>
   );
+};
+
+SearchBar.propTypes = {
+  setUser: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
