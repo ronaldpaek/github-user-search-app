ace1ab4764e84cd6c3abb5dbe18eeb2d16c251d5
Ronald Paek <ronaldpaek@gmail.com>
2023-08-28 21:16:38 -0700

    Update semantic tags for Header and UserCard

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,7 @@
 const AppHeader = () => {
   return (
     <header className="app-header">
-      <h1 className="app-title">devfinder</h1>
+      <h2 className="app-title">devfinder</h2>
       <div className="theme-toggle">
         <p className="theme-label">Light</p>
         <img className="theme-icon" src="/icon-sun.svg" alt="Sun icon" />
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,23 +1,33 @@
 import PropTypes from "prop-types";
 
-const UserCard = ({ name, handle, joinDate }) => {
+import { formatUrl } from "../utils";
+
+const UserCard = ({ name, handle, html_url, joinDate }) => {
   return (
-    <div className="user-card">
+    <section className="user-card">
       <img className="user-avatar" src="/avatar.svg" alt="User avatar" />
       <div className="user-details">
-        <h2 className="user-name">{name}</h2>
-        <p className="user-handle">{handle}</p>
+        <h2 className="user-name">{name === null ? "Not Available" : name}</h2>
+        <a
+          className="user-handle"
+          href={formatUrl(html_url)}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          @{handle}
+        </a>
         <time className="joined-date" dateTime={joinDate}>
           Joined {new Date(joinDate).toLocaleDateString()}
         </time>
       </div>
-    </div>
+    </section>
   );
 };
 
 UserCard.propTypes = {
   name: PropTypes.string.isRequired,
   handle: PropTypes.string.isRequired,
+  html_url: PropTypes.string.isRequired,
   joinDate: PropTypes.string.isRequired,
 };
 
