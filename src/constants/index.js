ded7487a190c29402e19bb587a1efc654c1a59ce
Ronald Paek <ronaldpaek@gmail.com>
2023-09-01 01:28:25 -0700

    Move default user data to constants folder

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+const DEFAULT_USER_DATA = {
+  avatar_url: "/avatar.svg",
+  name: "The Octocat",
+  login: "octocat",
+  html_url: "https://github.com/octocat",
+  created_at: "2011-01-25T18:44:36Z",
+  bio: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.",
+  public_repos: 8,
+  followers: 10288,
+  following: 9,
+  location: "San Francisco",
+  blog: "https://github.blog",
+  twitter_username: null,
+  company: "@github",
+};
+
+export { DEFAULT_USER_DATA };
