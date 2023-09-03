e1e25f66b5943240f820d1301736b26107c3fb12
Ronald Paek <ronaldpaek@gmail.com>
2023-09-02 23:17:34 -0700

    Add icons as reusable react components

---
+++ src/components/icons/index.js
@@ -0,0 +1,8 @@
+import Location from "./Location";
+import Website from "./Website";
+import Twitter from "./Twitter";
+import Company from "./Company";
+import Moon from "./Moon";
+import Sun from "./Sun";
+
+export { Location, Website, Twitter, Company, Moon, Sun };
