20dbc1b2b4f80034081d396c66e675e4d8f796e7
Ronald Paek <ronaldpaek@gmail.com>
2023-09-02 23:52:09 -0700

    Remove import statement

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,6 @@ import UserBio from "./UserBio";
 import UserStats from "./UserStats";
 import UserStat from "./UserStat";
 import UserContactDetails from "./UserContactDetails";
-import { Location } from "./Icons";
 
 export {
   AppHeader,
